5c5a1fb844ca695fdd0c254e28cdb922ceed1b52
Christopher Warner <christopher.warner92@gmail.com>
2020-10-27 17:52:46 -0400

    Created VideoChatContainer component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import VideoChatContainer from './Components/VideoChatContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Video Chat App</h1>
+      <h2>WebRTC + Firebase</h2>
+      <VideoChatContainer />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyC1O3pc7AQ1SOVUU0u6_7qfeIuVrUGCEVw",
+    authDomain: "serverless-video-chat.firebaseapp.com",
+    databaseURL: "https://serverless-video-chat.firebaseio.com",
+    projectId: "serverless-video-chat",
+    storageBucket: "serverless-video-chat.appspot.com",
+    messagingSenderId: "728740478864",
+    appId: "1:728740478864:web:fc584331943322ea6bfd15",
+    measurementId: "G-KTEF569GD0"
+  };
+
+  export default config;
