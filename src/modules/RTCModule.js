586996dc0667f866d928f7b36ada0e6478915bde
Christopher Warner <christopher.warner92@gmail.com>
2020-11-03 15:07:00 -0500

    Added fierbase and RTCModule.

---
+++ src/modules/RTCModule.js
@@ -0,0 +1,45 @@
+
+export const createOffer = async (connection, localStream , userToCall, doOffer, database, username) => {
+    try {
+
+    } catch (exception) {
+        console.error(exception);
+    }
+}
+
+export const initiateLocalStream = async () => {
+    try {
+
+    } catch (exception) {
+        console.error(exception);
+    }
+}
+
+export const initiateConnection = async () => {
+    try {
+        // create a connection
+        // using Google public stun server
+
+    } catch (exception) {
+        console.error(exception);
+    }
+}
+
+export const listenToConnectionEvents = (conn, username, remoteUsername, datebase, remoteVideoRef, doCandidate) => {
+    // listen for ice candidates
+
+    // when a remote user adds stream to the peer connection, we display it
+}
+
+export const sendAnswer = async (conn, localStream, notif, doAnswer, database, username) => {
+    try {
+        // add the localstream to the connection
+        // set the remote and local descriptions and create an answer
+
+        // create an answer to an offer
+
+        // send answer to the other peer
+    } catch (exception) {
+        console.error(exception);
+    }
+}
