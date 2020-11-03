12fb264778661c7756389d8a82e8f9a412ae8396
Christopher Warner <christopher.warner92@gmail.com>
2020-11-03 15:12:00 -0500

    sketched out FirebaseModule.

---
+++ src/modules/FirebaseModule.js
@@ -0,0 +1,15 @@
+export const doLogin = async (username, database, handleUpdate) => {
+
+}
+
+export const doOffer = async (to, offer, database, username) => {
+
+}
+
+export const doAnswer = async (to, answer, database, username) => {
+
+}
+
+export const doCandidate = async (to, candidate, database, username) => {
+    // send the new candidate to the peer
+}
